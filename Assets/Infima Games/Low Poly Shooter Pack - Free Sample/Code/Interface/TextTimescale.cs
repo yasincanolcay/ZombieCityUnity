95d7625958b29b11c1760ed236cb827193735217
Yasincan olcay <78057612+yasincanolcay@users.noreply.github.com>
2022-03-23 03:47:59 +0300

    Test zombie lowpoly game

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Interface/TextTimescale.cs
@@ -0,0 +1,21 @@
+﻿// Copyright 2021, Infima Games. All Rights Reserved.
+
+using UnityEngine;
+
+namespace InfimaGames.LowPolyShooterPack.Interface
+{
+    /// <summary>
+    /// Component that changes a text to match the current time scale.
+    /// </summary>
+    public class TextTimescale : ElementText
+    {
+        #region METHODS
+
+        protected override void Tick()
+        {
+            
+        }        
+
+        #endregion
+    }
+}
